e0d5fef2a3fbdb89645bf8f4000f3a7fa4ba0fb4
akram <akram.tj1@gmail.com>
2023-02-01 18:36:30 +0500

    comment

--- main.go
+++ main.go
@@ -6,5 +6,6 @@ import (
 )
 
 func main() {
+	// TotalAmount with parameters: productName, amount, phoneNumber, duration
 	fmt.Println("Total amount:", solve.TotalAmount("phone", 1000, "+992 93 988 88 88", 18))
 }
